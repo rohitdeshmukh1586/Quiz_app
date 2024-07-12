2e776677cbc23e3e2a62bf8d614fabb4f38549a9
git <rd9351801@gmail.com>
2024-07-13 01:36:23 +0530

    This Is My First Commit

---
+++ app/src/main/java/com/example/quiz_app/QuestionAnswer.java
@@ -0,0 +1,47 @@
+package com.example.quiz_app;
+
+public class QuestionAnswer {
+
+    public static String question []={
+            "Which of the following option leads to the portability and security of Java?",
+            "Which of the following is not a Java features?",
+            " _____ is used to find and fix bugs in the Java programs?",
+            "What is the return type of the hashCode() method in the Object class?",
+            "Which of the following tool is used to generate API documentation in HTML format from doc comments in source code?",
+            "Which of the following is an immediate subclass of the Panel class?",
+            "Which of the following is a reserved keyword in Java?",
+            "What is the initial quantity of the ArrayList list?",
+            "Who invented Java Programming?",
+            " Which of these cannot be used for a variable name in Java?",
+
+
+    };
+    public static String choices [][]={
+            {"Bytecode is executed by JVM" , "The applet makes the Java code secure and portable" , "Use of exception handling" , "Dynamic binding between objects"},
+            {"Dynamic" , "Architecture Neutral" , "Use of pointers" , "None of the above"},
+            {"JDB" , "JDK" , "JRV" , "JWM"},
+            {"Object" , "int" , "long" , "void"},
+            {"javap tool" , "java command" , "javadoc tool" , "javah command"},
+            {"applet class" , "windows class" , "frame class" , "dialog class"},
+            {"object" , "main" , "system" , "strictfp"},
+            {"5" , "10" , "0" , "100"},
+            {"Guido van Rossum" , "James Gosling" , "Dennis Ritchie" , "Bjarne Stroustrup"},
+            {"identifier & keyword" , "identifier" , "keyword" , "none of the mentioned"},
+
+    };
+    public static String correctAnswers []={
+            "Bytecode is executed by JVM" ,
+            "Use of pointers",
+            "JDB",
+            "int",
+            "javadoc tool",
+            "applet class",
+            "strictfp",
+            "10",
+            "James Gosling",
+            "keyword",
+
+
+
+    };
+}
